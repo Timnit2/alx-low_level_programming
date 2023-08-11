7e4f85405b478b4dd063f994de5d4e1eaf8fd45d
timnit kifle <tahgaus13@.com>
2023-08-11 07:06:37 -0700

    printing letter except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description : printing all samll letter except qand e
+ * Return: always 0
+ */
+int main(void)
+{
+	char smal;
+
+	while (smal <= 'z')
+	{
+		if (smal != 'e' && smal != 'q')
+		{
+			putchar(smal);
+		}
+		smal++;
+	}
+	putchar('\n');
+	return (0);
+}
