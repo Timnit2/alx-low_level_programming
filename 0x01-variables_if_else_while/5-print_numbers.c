05b8d5fb343fac3f3ef82c4f1bfbfe948c626f64
timnit kifle <tahgaus13@.com>
2023-08-11 07:24:46 -0700

    printing no till 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description : printing number till 10
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num == o; num < 10; num++)
+	{
+		putchar(num++);
+	}
+	putchar('\n');
+	return (0);
+}
