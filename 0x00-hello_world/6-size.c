b4064dc0f8f68a847ae79b505da5f59fd2312c8b
timnit kifle <tahgaus13@.com>
2023-08-12 00:34:21 -0700

    printng size of various data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * descrption : printing all sizeof datatype
+ * Return: always 0
+ */
+int main(0)
+{
+	printf("size of int = " + sizeof(int) + " bytes \n");
+	printf("size of char = " + sizeof(char) + " bytes \n");
+	printf("size of long = " + sizeof(long) + "bytes\n");
+	printf("size of float = " + sizeof(float) + "bytes\n");
+	printf("size of double = " + sizeof(double) + "bytes\n");
+}
