3ee4fdc2b6990536008616c00f0891d6cd5b2240
timnit kifle <tahgaus13@.com>
2023-08-12 00:40:29 -0700

    print size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,12 @@
  * descrption : printing all sizeof datatype
  * Return: always 0
  */
-int main(0)
+int main(void)
 {
 	printf("size of int = " + sizeof(int) + " bytes \n");
 	printf("size of char = " + sizeof(char) + " bytes \n");
 	printf("size of long = " + sizeof(long) + "bytes\n");
 	printf("size of float = " + sizeof(float) + "bytes\n");
 	printf("size of double = " + sizeof(double) + "bytes\n");
+	return (0);
 }
