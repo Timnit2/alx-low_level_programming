6de9dbe17073eb3fb90a78bd014b0ad23006ae57
timnit kifle <tahgaus13@.com>
2023-08-11 23:48:29 -0700

    printf text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * descrrption : prting  text
+ * Return: alays 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
