45ca5edbe2b0cbe032cff1e37910e034b2671078
timnit kifle <tahgaus13@.com>
2023-08-12 01:51:00 -0700

    printf size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(in));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(in));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
