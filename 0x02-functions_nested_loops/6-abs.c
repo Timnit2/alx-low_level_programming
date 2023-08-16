dc052ac833ea37ba78b03dfc821811f1457a3da5
timnit kifle <tahgaus13@.com>
2023-08-16 05:34:44 -0700

    absolute value of no

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of no
+ * @n: the int to check no
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
