bb32a92cc86e4a4de82e1d7867d8a30ace39cb92
timnit kifle <tahgaus13@.com>
2023-08-16 06:42:53 -0700

    adding two no

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adding two value
+ * @num1: the first num
+ * @num2: the second num
+ * Return: sum no
+ */
+int add(int num1, int num2)
+{
+	return (num + num2);
+}
