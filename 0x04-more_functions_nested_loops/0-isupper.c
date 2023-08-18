aaaf3593215b4717499ab0d927006c43d99f26da
timnit kifle <tahgaus13@.com>
2023-08-18 03:05:54 -0700

    checking uppercharacter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - cheking uppercase character
+ * @c:cheking the character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
