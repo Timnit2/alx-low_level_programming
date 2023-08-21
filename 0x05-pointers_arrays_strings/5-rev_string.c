7559c73187d40e784ff92b80dc8843c31241b96d
timnit kifle <tahgaus13@.com>
2023-08-21 13:38:40 -0700

    string reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: string pointer
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = strlen(s) - 1;
+	char temp;
+
+	while (a < b)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+		a++;
+		b--;
+	}
+}
