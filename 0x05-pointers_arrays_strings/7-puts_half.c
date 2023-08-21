67ddd0019b1e2679b953976f00dc880fe22842d8
timnit kifle <tahgaus13@.com>
2023-08-21 14:25:23 -0700

    printing half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - printing half of string
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+	int lengths = strlen(str);
+	int a = (lengths - 1) / 2 + 1;
+
+	while (a < lengths)
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
