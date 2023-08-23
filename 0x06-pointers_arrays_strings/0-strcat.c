85a219d25579549e4964b2db1c0cd23b40d01ed9
timnit kifle <tahgaus13@.com>
2023-08-23 02:58:36 -0700

    concanate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates
+ * @src: pointer string
+ * @dest: pointers string
+ * Return:len2
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0;
+	int len2 = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+	while (src[len2] != '\0')
+	{
+		dest[len1] = src [len2];
+		len1++;
+		len2++;
+	}
+	dest[len] = '\0';
+	return dest;
+}
