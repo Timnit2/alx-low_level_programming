6711fc2ebbb884fd1aed75f73c61eaa167168049
timnit kifle <tahgaus13@.com>
2023-08-24 05:26:21 -0700

    copies a string like strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0;
+	int len;
 
-	while (len < n && src[len] != '\0')
-	{
+	for (len = 0; len < n && src[len] != '\0'; len++)
 		dest[len] = src[len];
-		len++;
-	}
-	while (len < n)
-	{
-		dest[len] = '\n';
-		len++;
-	}
+	for ( ; len < n; len++)
+		dest[len] = '\0';
 	return (dest);
 }
