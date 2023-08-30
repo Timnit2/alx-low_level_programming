7229124f445203c90171544342f79483a1d5a5e7
timnit kifle <tahgaus13@.com>
2023-08-30 07:17:09 -0700

    root of natural square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int res_sqrt_recursion(int n, int a);
+/**
+ * _sqrt_recursion - printing the root no
+ * @n:the no that will chekh
+ * Return: root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (res_sqrt_recursion(int n, 0));
+}
+/**
+ * res_sqrt_recursion - calculate the root square no
+ * @a: root no
+ * @n: natural no
+ * Return: the result square root of n
+ */
+int res_sqrt_recursion(int n, int a)
+{
+	if (a * a > n)
+		return (-1);
+	if (a * a == n)
+		return (a);
+	return (res_sqrt_recursion(int n, a + 1));
+}
+
