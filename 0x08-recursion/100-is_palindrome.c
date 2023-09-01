1442493175b4a055d0b857635885da107670b5d2
timnit kifle <tahgaus13@.com>
2023-09-01 10:37:23 -0700

    cheking the first and the last if they eqaul

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * is_palindrome - to check whether the first and the last character are equal
+ * @s:is a pointer of string
+ * Return: 1 if if a string is palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int i = strlen(s);
+
+	if (i <= 1)
+	{
+		return (1);
+	}
+	if (s[0] != s[i - 1])
+	{
+		return (0);
+	}
+	s[i - 1] = '\0';
+	return (is_palindrome($s[1]));
+}
