e83d0fa339498fa923a2ce6f4af61def748b5433
timnit kifle <tahgaus13@.com>
2023-09-04 12:47:30 -0700

    creating array of char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *  *create_array -create an array of char
+ *
+ * @size: size of an array
+ * @c: is the character with which array
+ * Return: NULL if size 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *arryname = malloc(size * sizeof(char));
+
+	if (arryname == NULL)
+	{
+		return (NULL);
+	}
+	for (unsigned int a = 0; a < size; a++)
+	{
+	arryname[a] = c;
+	}
+return (arryname);
+}
