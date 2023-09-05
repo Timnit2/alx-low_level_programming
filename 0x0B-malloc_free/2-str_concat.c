9a7342249189557b6a975f53d37f5f2f706f4e3b
timnit kifle <tahgaus13@.com>
2023-09-05 01:49:39 -0700

    concanating two string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	lengths1 = strlen(s1);
 	lengths2 = strlen(s2);
 
-	char *new_result = malloc(lengths1 + lengths2 + 1);
+	*new_result = malloc(lengths1 + lengths2 + 1);
 
 	if (new_result == NULL)
 	{
