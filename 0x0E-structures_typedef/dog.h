a651437f7debe4552406c43112a3b15a2d881e3e
timnit kifle <tahgaus13@.com>
2023-09-12 13:25:23 -0700

    defing dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DEFINING_H
+#define DEFINING_H
+/**
+ * struct dog - description dog
+ * @name: pointer to the name of the dog
+ * @age: age of the dolg
+ * @owner: the owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
